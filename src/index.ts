77777b301d843d9ae85e808ef2eddd93f1f0bdfa
민 <alsrb4171@gmail.com>
2022-06-24 00:04:52 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import ready from "./listeners/ready";
 import interactionCreate from "./listeners/interactionCreate";
 import message from "./listeners/message";
 
-const token = "OTg4MDM5MzI4NDY4OTcxNTYw.GH9fsB.TDR-7_ZMcgybW_QydvWBGySexiZTYx3uvw2-Zw";
+const token = "your token";
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
